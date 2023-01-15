259ea4eaf24b08dacb2c7758790924735dd24bf3
Iris <iris.thuo@student.moringaschool.com>
2023-01-15 16:37:01 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import {useState} from 'react'
+
+
+function EyesOnMe(){
+ 
+    const [clubEyes, setclubEyes] = useState()
+    function handleFocus(event){
+        event.preventDefault()
+       console.log("Good!")
+       setclubEyes()
+    }
+
+    function handleBlur(event){
+        event.preventDefault()
+        console.log("Hey! eyes on me!")
+        setclubEyes()
+    }
+
+    return(
+        <div>
+            <button id="clubEyes" onFocus={handleFocus} onBlur={handleBlur}>Eyes On Me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,24 @@
 // Code Keypad Component Here
+import {useState} from 'react'
+
 
 function Keypad (){
+    const [passcode ,setPasscode] = useState("")
+    function handleClick(event){
+            event.preventDefault()
+        console.log('Entering password...')
+        setPasscode('Entering password...')
+        
+       
+    }
     return (
-        <div></div>
+        <div>
+            <form >
+                <label htmlFor="passcode">Passcode</label>
+            <input type="text" id="passcode" value={passcode} onChange={(e) => setPasscode(e.target.value)}/>
+            <button onClick={handleClick}>ENTER</button>
+            </form>
+        </div>
     )
 }
 
